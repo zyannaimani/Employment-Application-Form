1769a7359185b6a9ecc8713c8b24efb842317f43
Zyanna Imani <zyannaimani@gmail.com>
2020-07-07 13:30:49 -0400

    redesign layout

---
+++ script.js
@@ -0,0 +1,7 @@
+var startAppBtn = document.getElementById("start-app-btn")
+var notice = document.getElementById("notice")
+var application = document.getElementById("application")
+
+startAppBtn.addEventListener("click",()=>{
+    location.href="application.html"
+})
